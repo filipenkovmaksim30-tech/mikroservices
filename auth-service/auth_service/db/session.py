cc12ecb1619f5f82bc45f5c3b5e1630494ecd573
katasaevvv <filipenkov.mmm1@gmail.com>
2026-09-17 20:46:41 +0300

    refactor: create_async_engine params

--- auth-service/auth_service/db/session.py
+++ auth-service/auth_service/db/session.py
@@ -6,7 +6,7 @@ from auth_service.config import Settings
 
 settings = Settings()
 
-async_engine = create_async_engine(url=settings.postgresql_url, echo=False)
+async_engine = create_async_engine(url=settings.postgresql_url, echo=False, pool_pre_ping=True)
 async_session_factory = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
 async def get_session() -> AsyncIterator[AsyncSession]:
--- catalog-service/catalog_service/db/session.py
+++ catalog-service/catalog_service/db/session.py
@@ -6,7 +6,7 @@ from catalog_service.config import Settings
 
 settings = Settings()
 
-async_engine = create_async_engine(url=settings.postgresql_url, echo=True)
+async_engine = create_async_engine(url=settings.postgresql_url, echo=False, pool_pre_ping=True)
 async_session_factory = async_sessionmaker[AsyncSession](
     bind=async_engine, 
     expire_on_commit=False
